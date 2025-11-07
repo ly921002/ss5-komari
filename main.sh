a5cfc2d8f9a86b2b6f49108372d2bda7cd2d0496
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-11-07 16:09:15 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 # ==================================================
 
 export AGENT_ENDPOINT=${AGENT_ENDPOINT:-"https://gcp.240713.xyz"}
-export AGENT_TOKEN=${AGENT_TOKEN:-"rDQfTqjDltC3zuvCPLJdJ0"}
+export AGENT_TOKEN=${AGENT_TOKEN:-"2wUkv6P5TWhZkbbQpYjIis"}
 export SOPT=${SOPT:-"8888"}
 export UUID=${UUID:-"0000"}
 

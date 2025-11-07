c83a8855a5382aaf8adc2fa97d1bcd19737e927f
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-11-07 16:08:09 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export AGENT_ENDPOINT=${AGENT_ENDPOINT:-"https://gcp.240713.xyz"}
+export AGENT_TOKEN=${AGENT_TOKEN:-"rDQfTqjDltC3zuvCPLJdJ0"}
+export SOPT=${SOPT:-"8888"}
+export UUID=${UUID:-"0000"}
+
+# 添加指定的命令
+sopt="$SOPT" uuid="$UUID" bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh)
+
+# 下载 agent
+curl -sL -o komari-agent https://raw.githubusercontent.com/ly921002/gcp/refs/heads/main/komari-agent-linux-amd64 
+chmod 755 komari-agent
+
+# 使用环境变量运行命令
+nohup ./komari-agent \
+    -e "$AGENT_ENDPOINT" \
+    -t "$AGENT_TOKEN" \
+    > komari-agent.log 2>&1 &
+
+# 显示日志文件路径
+echo "Application started in background"
+echo "Logs are being written to: komari-agent.log"
+
+# 保持容器运行
+tail -f /dev/null
